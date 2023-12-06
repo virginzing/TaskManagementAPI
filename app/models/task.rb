f5332b8b6571ac61ba2011cdef6f7df3b102276c
Tadthep Kanlayanapark <virginzing@gmail.com>
2023-12-06 17:01:49 +0700

    fixed as json for API

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,22 @@ class Task < ApplicationRecord
 
   belongs_to :created_by, class_name: "User"
   belongs_to :last_updated_by, class_name: "User"
+
+  validates :title, presence: true
+
+  def as_json(options = nil)
+    {
+      id: id,
+      title: title,
+      description: description,
+      due_date: due_date,
+      status: status,
+      created_by: created_by,
+      last_updated_by: last_updated_by,
+      created_at: created_at,
+      updated_at: updated_at,
+      deleted_at: deleted_at,
+      changelogs: changelogs
+    }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def as_json(options = nil)
+    {
+      id: id,
+      email: email
+    }
+  end
 end
